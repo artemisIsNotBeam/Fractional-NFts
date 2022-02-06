8156b455b9994372888f3af3bab55ac2b52696ef
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-02-06 07:01:00 -0800

    Just created
    created !!!

---
+++ contract.sol
@@ -0,0 +1,7 @@
+//hey welcome to my contract
+//da idea is very simple
+//take NFT,
+//spit out x NFT
+//make it where you can always claim the NFT by giving the 
+// parts back
+pragma solidity >=0.7.0 <0.9.0;
