70e2287927caa88138f42bfca7fb9a38e40479c9
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-02-08 20:25:15 -0800

    contract

--- contract.sol
+++ contract.sol
@@ -1,7 +1,15 @@
 //hey welcome to my contract
-//da idea is very simple
+//da contracrt is very simple
 //take NFT,
 //spit out x NFT
 //make it where you can always claim the NFT by giving the 
 // parts back
 pragma solidity >=0.7.0 <0.9.0;
+
+contract fractionalNFTs{
+    struct NFTpair{
+        address NFTcontract;
+        uint amountOfSPlits;
+        uint[] splitIndex;
+    }
+}
